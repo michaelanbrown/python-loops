0a1f6b3aa6182eb494bc3d7d6bc3e4eef1a175a7
michaelanbrown <m_brownbrown@yahoo.com>
2024-08-19 12:32:02 -0400

    passed last test

--- lib/looping.py
+++ lib/looping.py
@@ -12,5 +12,12 @@ def square_integers(int_list):
     return [i ** 2 for i in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
